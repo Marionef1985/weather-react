afd562ccf97491abd4e20b2090dbb17faa248818
Marionef1985 <marionef1985@gmail.com>
2020-12-03 19:22:33 +0100

    Integrated the Serach component inside the FutureCard component

--- src/Futurecard.js
+++ src/Futurecard.js
@@ -1,12 +1,35 @@
 import React from "react";
-import Search from "./Search";
 import "./Futurecard.css";
 
 export default function Futurecard() {
   return (
     <div>
       <div className="card-body">
-        <Search />
+        <div className="row">
+        <div className="col-12">
+          <form id="searchCity">
+            <div className="form-group search-city">
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputtext1"
+                aria-describedby="textHelp"
+                placeholder="Search City"
+                autocomplete="off"
+                autoFocus="on"
+              />
+            </div>
+          </form>
+          <div className="col-12">
+            <button className="btnCurrent" id="btn-current">
+              Current
+            </button>
+            <button className="btnGo" id="btn-go">
+              Go
+            </button>
+          </div>
+        </div>
+      </div>
         <br />
         <section className="col-12 second-card">
           <div className="row">
